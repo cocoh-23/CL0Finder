52ecb9d0b3d23b6cc2923a5b9ee446fc0b368e49
cocoh_23 <62628345+cocoh-23@users.noreply.github.com>
2023-03-24 14:16:59 -0300

    Update FindCL0CandidateURLs.py

--- FindCL0CandidateURLs.py
+++ FindCL0CandidateURLs.py
@@ -23,16 +23,17 @@ session.stream = False
 URLs = open(URLFilePath, "r")
 results = open(ResultsFilePath, "a")
 for url in URLs:
+    url = url.rstrip()
     GetResponseCodeTest = session.get(url, allow_redirects=False).status_code
     PostResponseCodeTest = session.post(url, allow_redirects=False).status_code
     PutResponseCodeTest = session.put(url, allow_redirects=False).status_code
-    requestQualifiesForCL0 = (GetResponseCodeTest > 300 and GetResponseCodeTest < 404) or (PostResponseCodeTest > 300 and PostResponseCodeTest < 404) or (PutResponseCodeTest > 300 and PutResponseCodeTest < 404)
+    requestQualifiesForCL0 = ((GetResponseCodeTest > 300 and GetResponseCodeTest < 404) or GetResponseCodeTest == 422) or ((PostResponseCodeTest > 300 and PostResponseCodeTest < 404) or PostResponseCodeTest == 422) or ((PutResponseCodeTest > 300 and PutResponseCodeTest < 404) or PutResponseCodeTest == 422)
     if(requestQualifiesForCL0):
-        if(GetResponseCodeTest != 405 and (GetResponseCodeTest > 300 and GetResponseCodeTest < 404) or GetResponseCodeTest == 422 ):
-            results.write(url + " - GET")
-        if(PostResponseCodeTest != 405 and (PostResponseCodeTest > 300 and PostResponseCodeTest < 404) or PostResponseCodeTest == 422 ):
-            results.write(url + " - POST")
-        if(PutResponseCodeTest != 405 and (PutResponseCodeTest > 300 and PutResponseCodeTest < 404) or PutResponseCodeTest == 422 ):
-            results.write(url + " - PUT")
+        if((GetResponseCodeTest > 300 and GetResponseCodeTest < 404) or GetResponseCodeTest == 422 ):
+            results.write(url + "-GET-" + str(GetResponseCodeTest) + "\n")
+        if((PostResponseCodeTest > 300 and PostResponseCodeTest < 404) or PostResponseCodeTest == 422 ):
+            results.write(url + "-POST-" + str(PostResponseCodeTest) + "\n")
+        if((PutResponseCodeTest > 300 and PutResponseCodeTest < 404) or PutResponseCodeTest == 422 ):
+            results.write(url + "-PUT-" + str(PutResponseCodeTest) + "\n")
 results.close()
 URLs.close()
