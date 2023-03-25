ff945d090ba5d1ffdf38d3406a01fa78637a8128
cocoh_23 <62628345+cocoh-23@users.noreply.github.com>
2023-03-25 19:49:47 -0300

    Update CL0Finder.py

--- CL0Finder.py
+++ CL0Finder.py
@@ -43,12 +43,14 @@ URLs = open(URLFilePath, "r")
 for url in URLs:
     CandidateMethod = url.split("---")[1]
     FullUrl = parse(url.split("---")[0])
-    NormalRespCode = url.split("---")[2]
+    NormalRespCode = int(url.split("---")[2])
     CL0Scheme = FullUrl.scheme
     CL0Host = FullUrl.netloc
     CL0TestPath = FullUrl.path
     CL0ResultPath = FullUrl.path
     CL0TestBody = 'GET /thisPageDoesNotExist HTTP/1.1\r\nFoo: Bar'
+    CL0GetPostResult = ''
+    CL0GetPutResult = ''
     if(CandidateMethod == 'GET'):#In case of get, we try to desync, but smuggling with other methods too
         CL0Test = session.get(CL0Scheme + "://" + CL0Host + CL0TestPath, data=CL0TestBody, allow_redirects=False)
         CL0Result = session.get(CL0Scheme + "://" + CL0Host + CL0ResultPath, allow_redirects=False)
@@ -63,8 +65,10 @@ for url in URLs:
         CL0Test = session.put(CL0Scheme + "://" + CL0Host + CL0TestPath, data=CL0TestBody, allow_redirects=False)
         CL0Result = session.put(CL0Scheme + "://" + CL0Host + CL0ResultPath, allow_redirects=False)
     if(NormalRespCode != CL0Result.status_code):
-        print("The response code for %s is %s and after CL0 is %s for method %s" % (CL0TestPath,str(NormalRespCode.status_code),str(CL0Result.status_code),CandidateMethod))
-    if(NormalRespCode != CL0GetPostResult.status_code):
-        print("The response code for %s is %s and after CL0 is %s for method POST ,but using a GET to smuggle the request" % (CL0TestPath,str(NormalRespCode.status_code),str(CL0GetPostResult.status_code)))
-    if(NormalRespCode != CL0GetPutResult.status_code):
-        print("The response code for %s is %s and after CL0 is %s for method PUT ,but using a GET to smuggle the request" % (CL0TestPath,str(NormalRespCode.status_code),str(CL0GetPutResult.status_code)))
+        print("The response code for %s is %s and after CL0 is %s for method %s" % (CL0TestPath,str(NormalRespCode),str(CL0Result.status_code),CandidateMethod))
+    if(CL0GetPostResult !='' and NormalRespCode != CL0GetPostResult.status_code):
+        print("The response code for %s is %s and after CL0 is %s for method POST ,but using a GET to smuggle the request" % (CL0TestPath,str(NormalRespCode),str(CL0GetPostResult.status_code)))
+        CL0GetPostResult =''
+    if(CL0GetPutResult!='' and NormalRespCode != CL0GetPutResult.status_code):
+        print("The response code for %s is %s and after CL0 is %s for method PUT ,but using a GET to smuggle the request" % (CL0TestPath,str(NormalRespCode),str(CL0GetPutResult.status_code)))
+        CL0GetPutResult!=''
