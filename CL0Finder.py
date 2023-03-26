b31f8274a703917c69d5f8c2aff935b6f4f7ef10
cocoh_23 <62628345+cocoh-23@users.noreply.github.com>
2023-03-26 18:24:58 -0300

    Update CL0Finder.py

--- CL0Finder.py
+++ CL0Finder.py
@@ -35,40 +35,40 @@ URLFilePath = args.urlFile.strip()
 #Session config
 proxies = {'http': '127.0.0.1:8080','https': '127.0.0.1:8080'}
 session = requests.Session()
-#session.proxies = proxies
-session.allow_redirects = False
-session.stream = True
-
 URLs = open(URLFilePath, "r")
 for url in URLs:
+    session.proxies = proxies
+    session.stream = True
+    session.verify = False
     CandidateMethod = url.split("---")[1]
     FullUrl = parse(url.split("---")[0])
+    print(FullUrl)
     NormalRespCode = int(url.split("---")[2])
     CL0Scheme = FullUrl.scheme
     CL0Host = FullUrl.netloc
-    CL0TestPath = FullUrl.path
-    CL0ResultPath = FullUrl.path
+    CL0TestPath = CL0Scheme + "://" + CL0Host + FullUrl.path + "?" + FullUrl.query
     CL0TestBody = 'GET /thisPageDoesNotExist HTTP/1.1\r\nFoo: Bar'
     CL0GetPostResult = ''
     CL0GetPutResult = ''
+    headers = {"Content-Type" : "application/x-www-form-urlencoded"}
     if(CandidateMethod == 'GET'):#In case of get, we try to desync, but smuggling with other methods too
-        CL0Test = session.get(CL0Scheme + "://" + CL0Host + CL0TestPath, data=CL0TestBody, allow_redirects=False)
-        CL0Result = session.get(CL0Scheme + "://" + CL0Host + CL0ResultPath, allow_redirects=False)
-        CL0PostTest = session.post(CL0Scheme + "://" + CL0Host + CL0ResultPath,data=CL0TestBody, allow_redirects=False)
-        CL0GetPostResult = session.get(CL0Scheme + "://" + CL0Host + CL0TestPath, allow_redirects=False)
-        CL0PutTest = session.put(CL0Scheme + "://" + CL0Host + CL0ResultPath,data=CL0TestBody, allow_redirects=False)
-        CL0GetPutResult = session.get(CL0Scheme + "://" + CL0Host + CL0TestPath, allow_redirects=False)
+        CL0Test = session.get(CL0TestPath, data=CL0TestBody, allow_redirects=False,headers=headers)
+        CL0Result = session.get(CL0TestPath, allow_redirects=False)
+        CL0PostTest = session.post(CL0TestPath,data=CL0TestBody, allow_redirects=False,headers=headers)
+        CL0GetPostResult = session.get(CL0TestPath, allow_redirects=False)
+        CL0PutTest = session.put(CL0TestPath,data=CL0TestBody, allow_redirects=False,headers=headers)
+        CL0GetPutResult = session.get(CL0TestPath, allow_redirects=False)
     elif(CandidateMethod == 'POST'):
-        CL0Test = session.post(CL0Scheme + "://" + CL0Host + CL0TestPath, data=CL0TestBody, allow_redirects=False)
-        CL0Result = session.post(CL0Scheme + "://" + CL0Host + CL0ResultPath, allow_redirects=False)
+        CL0Test = session.post(CL0TestPath, data=CL0TestBody, allow_redirects=False,headers=headers)
+        CL0Result = session.post(CL0TestPath, allow_redirects=False)
     else: #PUT
-        CL0Test = session.put(CL0Scheme + "://" + CL0Host + CL0TestPath, data=CL0TestBody, allow_redirects=False)
-        CL0Result = session.put(CL0Scheme + "://" + CL0Host + CL0ResultPath, allow_redirects=False)
+        CL0Test = session.put(CL0TestPath, data=CL0TestBody, allow_redirects=False,headers=headers)
+        CL0Result = session.put(CL0TestPath, allow_redirects=False)
     if(NormalRespCode != CL0Result.status_code):
         print("The response code for %s is %s and after CL0 is %s for method %s" % (CL0TestPath,str(NormalRespCode),str(CL0Result.status_code),CandidateMethod))
     if(CL0GetPostResult !='' and NormalRespCode != CL0GetPostResult.status_code):
-        print("The response code for %s is %s and after CL0 is %s for method POST ,but using a GET to smuggle the request" % (CL0TestPath,str(NormalRespCode),str(CL0GetPostResult.status_code)))
+        print("The response code for %s is %s and after CL0 is %s for method GET ,but using a POST to smuggle the request" % (CL0TestPath,str(NormalRespCode),str(CL0GetPostResult.status_code)))
         CL0GetPostResult =''
     if(CL0GetPutResult!='' and NormalRespCode != CL0GetPutResult.status_code):
-        print("The response code for %s is %s and after CL0 is %s for method PUT ,but using a GET to smuggle the request" % (CL0TestPath,str(NormalRespCode),str(CL0GetPutResult.status_code)))
+        print("The response code for %s is %s and after CL0 is %s for method GET ,but using a PUT to smuggle the request" % (CL0TestPath,str(NormalRespCode),str(CL0GetPutResult.status_code)))
         CL0GetPutResult!=''
